4ac75046962431a82506903d0bfb7dbca477ca34
tharuszzz <rushinjhhh@gmail.com>
2025-03-10 19:23:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,6 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
   OWNER_NUM: process.env.OWNER_NUM || "94740326138",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "",
+  ALIVE_MSG: process.env.ALIVE_MSG || "",
 };
