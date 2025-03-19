7e399050a834444585f7476e0ada2210faabfa52
tharuszzz <rushinjhhh@gmail.com>
2025-03-19 17:42:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Vq9zWabJ#bC3K-FJiBZpZkwAFieullmkk_K5z_hHr068beVGfH5s",
   MONGODB: process.env.MONGODB || "mongodb://mongo:QmdsFSfYauBnDDcyWbQSDvFkxZYJyeEU@interchange.proxy.rlwy.net:11974",
   OWNER_NUM: process.env.OWNER_NUM || "94740326138",
+  PREFIX: process.env.PREFIX || ".",
 };
