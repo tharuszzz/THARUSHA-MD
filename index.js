ac53e983bc6f4a1b97a9c80bf636b00c28099725
tharuszzz <rushinjhhh@gmail.com>
2025-03-29 20:04:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ownerNumber = ['94740326138']
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("MALVIN-XD~", '');
+const sessdata = config.SESSION_ID.replace("THARUSHA~MD=", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
